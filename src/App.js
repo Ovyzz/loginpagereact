c3f36e3107001b12a4a4e7c37513d0dc3242502d
Ovyzz <ovy_43@yahoo.ro>
2022-02-04 10:15:54 +0200

    Up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Phone from "./design/iPhone Mockup.png"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="container">
+            <div className="first-box">
+                <h1>Gradie</h1>
+                <p>Beautiful gradients in seconds.</p>
+                <div className="phone-box">
+                    <img src={Phone} className="phone-img"/>
+                </div>
+            </div>
+            <div className="second-box">
+                <h1>Login</h1>
+                <form>
+                    <div className="form-box">
+                        <h2>Email</h2>
+                        <input type="email" placeholder="john@example.com"/>
+                    </div>
+                    <div className="form-box">
+                        <h2 type="password">Password</h2>
+                        <input
+                            type="password"
+                            placeholder="At least 8 characters"
+                        />
+                    </div>
+                    <div className="checkbox-box">
+                        <input type="checkbox" className="checkbox-round"/>
+                        <label>By creating an account, you agree to the <a href="#">Terms &
+                            Conditions.</a></label
+                        >
+                    </div>
+                    <div className="form-box">
+                        <input type="submit" className="form-btn" value="Create an Account"/>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
 }
 
 export default App;
